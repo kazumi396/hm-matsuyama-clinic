261e9515cde9658de81dfc6798363eb51cccd807
Kazumi Iitsuka <ii.k.ne.1111@gmail.com>
2025-09-21 16:50:11 +0900

    フォルダにデータを格納しました。

---
+++ js/main.js
@@ -0,0 +1,114 @@
+/**
+ * ビューポートの設定を切り替え
+ * 画面の幅が380px未満の場合：ビューポートを380pxに固定
+ * それ以上の場合：デバイスの幅に基づいてビューポートを設定
+ */
+const switchViewport = () => {
+  // ビューポート要素を取得
+  const viewportMeta = document.querySelector('meta[name="viewport"]');
+
+  // 条件に基づいて適用するビューポートの設定を決定
+  const viewportContent = window.outerWidth > 380 ? "width=device-width, initial-scale=1" : "width=380";
+
+  // ビューポート要素が存在しない場合はreturn
+  if (!viewportMeta) return;
+
+  // 現在のビューポートの設定が目的の設定と異なる場合にのみ、新しい設定を適用します。
+  if (viewportMeta.getAttribute("content") !== viewportContent) {
+    viewportMeta.setAttribute("content", viewportContent);
+  }
+};
+switchViewport();
+window.addEventListener("resize", switchViewport);
+
+/**
+ * ハンバーガーメニュー
+ */
+const menu = () => {
+  const menu = document.querySelector(".js-menu");
+  const button = document.querySelector(".js-button");
+  const closeButton = document.querySelector(".js-close-button");
+
+  // コンテンツ Opening Keyframe
+  const contentsOpeningKeyframes = {
+    opacity: [0, 1],
+  };
+
+  // コンテンツ Opening Option
+  const contentsOpeningOptions = {
+    duration: 200,
+    easing: "ease-out",
+  };
+
+  // コンテンツ Closing Keyframe
+  const contentsClosingKeyframes = {
+    opacity: [1, 0],
+  };
+
+  // コンテンツ Closing Option
+  const contentsClosingOptions = {
+    duration: 200,
+    easing: "ease-out",
+  };
+
+  // menuとbuttonがページ内にない場合returnする
+  if (!menu || !button) return;
+
+  // メニューopenする関数
+  const openMenu = () => {
+    menu.showModal();
+    menu.animate(contentsOpeningKeyframes, contentsOpeningOptions);
+  };
+
+  // メニューcloseする関数
+  const closeMenu = () => {
+    const closingAnim = menu.animate(contentsClosingKeyframes, contentsClosingOptions);
+
+    // アニメーションの完了後
+    closingAnim.onfinish = () => {
+      menu.close();
+    };
+  };
+
+  // ボタンクリックでopen
+  button.addEventListener("click", () => {
+    openMenu();
+  });
+
+  // クローズボタンクリックでclose
+  closeButton.addEventListener("click", () => {
+    closeMenu();
+  });
+
+  // Escapeキーを押すと非表示
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      event.preventDefault();
+      closeMenu();
+    }
+  });
+};
+
+menu();
+
+/**
+ * ファーストビュー　splide
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide", {
+    type: "loop",
+    perPage: 3,
+    breakpoints: {
+      768: {
+        perPage: 1,
+      },
+    },
+    autoplay: false,
+    interval: 3000,
+    pauseOnHover: true,
+    arrows: false,
+    pagination: false,
+    gap: "1rem",
+    padding: { left: "1rem", right: "1rem" },
+  }).mount();
+});
